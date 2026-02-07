767eb1eba3eb4e399eb1c09b6341f52a5909ef52
iustinzalog <iustinzalog@gmail.com>
2026-02-07 10:43:46 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+function toggleCheckbox() {
+  const checkbox = document.getElementById("sactive");
+  checkbox.checked = !checkbox.checked;
+  document
+    .querySelectorAll('input[type="checkbox"][class="cbgrup"]')
+    .forEach((checkbox) => {
+      checkbox.checked = false;
+    });
+}
+const container = document.getElementById("cont");
+container.querySelectorAll('input[type="checkbox"]').forEach((checkbox) => {
+  checkbox.addEventListener("change", function () {
+    const label = document.querySelector(`label[for="${this.id}"]`);
+    if (this.checked) {
+      label.textContent = "Citeste mai putin!";
+    } else {
+      label.textContent = "Vezi informatii suplimentare si imagini!";
+    }
+  });
+});
+function handleResize() {
+  const checkbox = document.getElementById("sactive");
+  if (window.innerWidth > 995) {
+    checkbox.checked = false;
+  }
+}
+window.addEventListener("resize", handleResize);
